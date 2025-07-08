450c6ebf1d4dcf28a65bb9fecee00afeab14b819
AmbitiousNOP <cjpenn7@icloud.com>
2025-07-08 19:26:52 -0400

    hyprland fix

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -1,6 +1,6 @@
 {pkgs, config, ...}: {
 
-  home.file.".config/hypr/hyprland.config".source = ./hyprland.conf;
+  home.file.".config/hypr/hyprland.conf".source = ./hyprland.conf;
   home.file.".config/hypr/hyprpaper.conf".source = ./hyprpaper.conf;
   home.file.".config/hypr/wallpaper" = {
       source = ./wallpaper;
