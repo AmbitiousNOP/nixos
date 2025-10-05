e10cfecd23c4ce43050a25d6d504a2ad76522f21
AmbitiousNOP <cjpenn7@icloud.com>
2025-10-05 02:00:10 -0400

    ez hardware

--- hosts/default.nix
+++
@@ -1,3 +0,0 @@
-# this file is necessary so nix-env -qa does not break,
-# when nixos-hardware is used as a channel
-{ }
--- hosts/thinkpad/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, ... }:
-
-{
-  imports = [ ../common/pc/laptop ];
-  hardware.trackpoint.enable = lib.mkDefault true;
-  hardware.trackpoint.emulateWheel = lib.mkDefault config.hardware.trackpoint.enable;
-}
