3b1aa0309ec9bb6b0059cd809ef0a5be0600bdff
AmbitiousNOP <cjpenn7@icloud.com>
2025-12-07 21:23:49 -0500

    security

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -1,4 +1,9 @@
-{pkgs, config, username, ...}:
+{
+  pkgs,
+  config,
+  username,
+  ...
+}:
 {
   programs = {
     firefox = {
@@ -6,42 +11,66 @@
       package = pkgs.librewolf;
       policies = {
         DisableTelemtry = true;
-	DisableFirefoxStudies = true;
-	Preferences = {
+        DisableFirefoxStudies = true;
+        Preferences = {
           "cookiebanners.service.mode.privateBrowsing" = 2; # Block cookie banners in private browsing
           "cookiebanners.service.mode" = 2; # Block cookie banners
-          "privacy.donottrackheader.enabled" = true;
+
           "privacy.fingerprintingProtection" = true;
           "privacy.resistFingerprinting" = true;
+          "privacy.resistFingerprinting.letterboxing" = true;
+
           "privacy.trackingprotection.emailtracking.enabled" = true;
           "privacy.trackingprotection.enabled" = true;
           "privacy.trackingprotection.fingerprinting.enabled" = true;
           "privacy.trackingprotection.socialtracking.enabled" = true;
-	};
-	ExtensionSettings = {
-	  "uBlock0@raymondhill.net" = {
-	    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
-	    installation_mode = "force_installed";
-	  };
-	  "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
-	    install_url = "https://addons.mozilla.org/firefox/downloads/file/4524018/vimium_ff-2.3.xpi";
-	    installation_mode = "force_installed";
-	  };
-	  "{74145f27-f039-47ce-a470-a662b129930a}" = {
-	    install_url = "https://addons.mozilla.org/firefox/downloads/file/4432106/clearurls-1.27.3.xpi";
-	    installation_mode = "force_installed";
-	  };
-	  "jid1-BoFifL9Vbdl2zQ@jetpack" = {
-	    install_url = "https://addons.mozilla.org/firefox/downloads/file/4392113/decentraleyes-3.0.0.xpi";
-	    installation_mode = "force_installed";
-	  };
-	  "jid1-MnnxcxisBPnSXQ@jetpack" = {
-	    install_url = "https://addons.mozilla.org/firefox/downloads/file/4504218/privacy_badger17-2025.5.30.xpi";
-	    installation_mode = "force_installed";
-	  };
-	};
+          "privacy.query_stripping.enabled" = true;
+          "privacy.query_stripping.enabled.pbmode" = true;
+
+          "privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts" = true;
+          "privacy.resistFingerprinting.randomDataOnCanvasExtract" = true;
+
+          "network.trr.mode" = 3;
+          "network.trr.custom_uri" = "https://base.dns.mullvad.net/dns-query";
+          "network.trr.uri" = "https://base.dns.mullvad.net/dns-query";
+          "network.http.referer.XOriginPolicy" = 2;
+
+          "browser.policies.runOncePerModification.setDefaultSearchEngine" = "DuckDuckGo Lite";
+          "dom.security.https_only_mode_ever_enabled" = true;
+          "security.tls.enable_0rtt_data" = false;
+        };
+        ExtensionSettings = {
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4524018/vimium_ff-2.3.xpi";
+            installation_mode = "force_installed";
+          };
+          "{74145f27-f039-47ce-a470-a662b129930a}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4432106/clearurls-1.27.3.xpi";
+            installation_mode = "force_installed";
+          };
+          "jid1-BoFifL9Vbdl2zQ@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4392113/decentraleyes-3.0.0.xpi";
+            installation_mode = "force_installed";
+          };
+          "jid1-MnnxcxisBPnSXQ@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4504218/privacy_badger17-2025.5.30.xpi";
+            installation_mode = "force_installed";
+          };
+          "{73a6fe31-595d-460b-a920-fcc0f8843232}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4628164/noscript-13.5.xpi";
+            installation_mode = "force_installed";
+          };
+          "CanvasBlocker@kkapsner.de" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/file/4413485/canvasblocker-1.11.xpi";
+            installation_mode = "force_installed";
+          };
+        };
       };
-      profiles.${username} = {};
+      profiles.${username} = { };
     };
   };
 
--- hosts/common/system.nix
+++ hosts/common/system.nix
@@ -57,6 +57,22 @@
   networking.hostName = hostname;
   networking.networkmanager.enable = true;
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.nameservers = [
+    "1.1.1.1#one.one.one.one"
+    "1.0.0.1#one.one.one.one"
+  ];
+  services.resolved = {
+    enable = true;
+    dnssec = "true";
+    domains = [ "~." ];
+    fallbackDns = [
+      "1.1.1.1#one.one.one.one"
+      "1.0.0.1#one.one.one.one"
+    ];
+    dnsovertls = "true";
+  };
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
